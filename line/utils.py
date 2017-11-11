74ca45f07abce3dd6eec3c99c8c31578769e3740
Jiarui XU <xvjiarui0826@gmail.com>
2017-11-11 09:14:02 +0800

    change token

--- line/utils.py
+++ line/utils.py
@@ -1,4 +1,4 @@
-channel_access_token = 'KNRFob43cptjHSZ3kfgqkb5MgDDXbtkhJDQDl5au0pq9c4oeitMXYAoK4wRd/jDu+NJv48dAQCdSOgLWQwOMC1B50j6REkm26uuuomB2ttARfWRiAOSrLz11GP3FGSloVpRbxl8T8he8egLU7XSiewdB04t89/1O/w1cDnyilFU='
+channel_access_token = 'abt2JsNhy4uK+Y0P0eK3Iw7YRr3pL6h3juizwDUUqYXqcPTjeDN59UIPCKhDCPy9W3dfiyx1SymkVcQordtrKlTmuPpSnl3DwcapyMUhlQiQK5FNGMji2UOu4QOGVxdmvD+T+KIB4ebY8pArg/kejQdB04t89/1O/w1cDnyilFU='
 
 import requests
 from itertools import repeat
@@ -18,9 +18,8 @@ from linebot.models import (
 
 def push_message_to_user(user, message):
 	post_data = {'receiver': user.id, 'message': message+user.name}
-	post_data['receiver'] = "Ufedbdb7c3d944c326a4251ac135b69e3"
 	try:
-		responese = requests.post("https://gentle-fjord-43717.herokuapp.com/push", data=post_data)
+		responese = requests.post("https://comp3111h-line-chatbot.herokuapp.com/push", data=post_data)
 		responese.raise_for_status()
 	except Exception as e:
 		print (e)
@@ -34,11 +33,9 @@ def push_message_to_users(user_ids, message):
 line_bot_api = LineBotApi(channel_access_token)
 
 def line_multicast(user_ids, message):
-	user_ids = ['Ufedbdb7c3d944c326a4251ac135b69e3']
 	line_bot_api.multicast(user_ids, TextSendMessage(text=message))
 
 def line_push(user_id, message):
-	user_id = 'Ufedbdb7c3d944c326a4251ac135b69e3'
 	line_bot_api.push_message(user_id, TextSendMessage(text=message))
 
 def line_map_push(user_ids, messages):
